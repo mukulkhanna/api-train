6e4b3c883396a84f22af12bb6f320e2de3b66ce3
Mukul Khanna <mukul18khanna@gmail.com>
2018-07-20 01:35:54 +0530

    🐞

--- public/src/components/info/got.vue
+++ public/src/components/info/got.vue
@@ -48,7 +48,7 @@
                             <v-btn icon v-clipboard:copy="card.link" @click="handleCopyStatus()"><v-icon color="pink">fa-clone</v-icon></v-btn>
                           </v-flex>
                           <v-flex xs2>
-                            <v-btn icon to="/got/quotes"><v-icon small color="pink">launch</v-icon></v-btn>
+                            <v-btn icon :href="card.link"><v-icon small color="pink">launch</v-icon></v-btn>
                           </v-flex>
                         </v-layout>
                       </h3>
